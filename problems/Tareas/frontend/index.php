8c523d8e32b7531100ee858c0a1baa134e8ecd54
DR0105 <36673480+DR0105@users.noreply.github.com>
2021-05-06 22:38:09 -0500

    Corrección modal

--- problems/Tareas/frontend/index.php
+++ problems/Tareas/frontend/index.php
@@ -1,14 +1,7 @@
-<?php
-include("../backend/response.php");
-$newObj = new Tarea();
-$emps = $newObj->getTareas();
-?>
 <!doctype html>
-<html lang="es">
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../css/styles.css ">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -39,50 +32,40 @@ $emps = $newObj->getTareas();
                     <th>Action</th>
                 </tr>
             </thead>
-            <tbody>
-                <?php foreach ($emps as $key => $emp) : ?>
-                    <tr>
-                        <td><?php echo $emp['id'] ?></td>
-                        <td><?php echo $emp['nombre'] ?></td>
-                        <td><?php echo $emp['descripcion'] ?></td>
-                        <td><?php echo $emp['estado'] ?></td>
-                        <td>
-                            <div class="btn-group" data-toggle="buttons"><a href="#" target="_blank" class="btn btn-warning btn-xs">Editar</a><a href="#" target="_blank" class="btn btn-danger btn-xs" onclick="borrar(<?php $emp['id'] ?>)">Borrar</a>
+            <tbody id="tarea_body">
 
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
             </tbody>
         </table>
     </div>
-    <button type="button" id="botonagregar" class="btn btn-primary" data-toggle="modal" data-target="#modalagregar" action-btn-value="add">
+    <button type="button" id="botonagregar" class="btn btn-primary" data-toggle="modal" data-target="#add_model" action-btn-value="add">
         Agregar tarea <i class="fa fa-plus"> </i>
     </button>
     <div id="msg"></div>
 
-    <div class="modal" id="modalagregar" role="dialog">
-        <div class="modal-dialog modal-md">
+    <div id="add_model" class="modal fade">
+        <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
                     <h4 class="modal-title">Agregar tarea</h4>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                 </div>
-                <div class="modal-body">
-                    <form method="post">
+                <div class="modal-body" id="body_tarea">
+                    <form method="post" id="frm_add">
+                        <input type="hidden" value="add" name="action" id="action">
+                        <input type="hidden" value="0" name="tarea_id" id="tarea_id">
                         <div class="form-group">
-                            <label for="nombre">Nombre</label>
-                            <input type="text" class="form-control" id="nombre" placeholder="Ingresa el nombre de la tarea" />
+                            <label for="nombre" class="control-label">Nombre:</label>
+                            <input type="text" class="form-control" id="nombre" name="nombre" />
                         </div>
                         <div class="form-group">
-                            <label for="inputEmail">Descripción</label>
-                            <input type="text" class="form-control" id="descripcion" placeholder="Ingresa la descripción" />
+                            <label for="descripcion" class="control-label">Descripción:</label>
+                            <input type="text" class="form-control" id="descripcion" name="descripcion" />
                         </div>
                     </form>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-success" id="guardar">Continuar<i class="fa fa-save"></i></a>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                    <button type="button" id="btn_add" class="btn btn-primary">Guardar</button>
                 </div>
             </div>
         </div>
